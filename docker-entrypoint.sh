6f4a8de5715bb214f60159dc23da94c98cf8eedc
Andriy Khomych <andriy.khomych@gmail.com>
2017-10-23 18:22:05 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,18 +15,14 @@ fi
 
 if [ -z "$USE_ONLY_CONFIGS" ]; then
 
-  # Log errors in FPM.
-  if [ "$PHP_FPM_LOG_ERRORS" -eq "1" ]; then
+  # Show PHP errors.
+  if [ "$PHP_SHOW_ERRORS" -eq "1" ]; then
     sed -i 's/^;php_flag[display_errors].*/php_flag[display_errors] = on/' /etc/php7/php-fpm.conf
+    sed -i 's/^display_errors.*/display_errors = on/' /etc/php7/php.ini
   fi
 
   if [ -n "$PHP_FPM_PORT" ]; then
     sed -i 's@^listen.*@'"listen  = ${PHP_FPM_PORT}"'@' /etc/php7/php-fpm.conf
-  fi
-  
-  # Show PHP errors.
-  if [ "$PHP_SHOW_ERRORS" -eq "1" ]; then
-    sed -i 's/^display_errors.*/display_errors = on/' /etc/php7/php.ini
   fi
 
   if [ -n "$PHP_MEMORY_LIMIT" ]; then

989113ed9d89361928cc064555edb67cdeac6e5c
Andriy Khomych <andriy.khomych@gmail.com>
2018-10-05 10:27:43 +0300

    Update composer_drush.sh

--- setup_extensions/composer_drush.sh
+++ setup_extensions/composer_drush.sh
@@ -4,10 +4,5 @@ wget https://getcomposer.org/installer && php installer
 mv composer.phar /usr/local/bin/composer
 ln -s /usr/local/bin/composer /usr/bin/composer
 
-cd /temp_docker
-git clone https://github.com/drush-ops/drush.git /usr/local/src/drush
-cd /usr/local/src/drush
-git checkout $1  #or whatever version you want. for example master
-ln -s /usr/local/src/drush/drush /usr/bin/drush
-composer install
+composer global require drush/drush:8.*
 drush --version

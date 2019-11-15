0ca48b8f255245e8ac6c4c2bcefd8c540526c40e
Andrii Khomych <andriy.khomych@gmail.com>
2019-11-15 14:58:35 +0200

    Fix php-7 drush version.

--- extensions/1_php/4_cli-tools/2_drush.sh
+++ extensions/1_php/4_cli-tools/2_drush.sh
@@ -3,4 +3,5 @@ composer global require consolidation/cgr;
 export PATH="$(composer config -g home)/vendor/bin:$PATH"
 export CGR_BIN_DIR=$HOME/bin
 COMPOSER_HOME=/opt/drush COMPOSER_BIN_DIR=/usr/local/bin COMPOSER_VENDOR_DIR=/opt/drush/"$DRUSH_VERSION" composer require drush/drush:^"$DRUSH_VERSION"
+cd /opt/drush/"$DRUSH_VERSION"/drush/drush/ && composer update
 chmod +x /opt

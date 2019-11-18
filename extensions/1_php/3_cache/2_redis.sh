1fd70092ca1fb808b5f6d7ba076685bbe7abf3e1
Andrii Khomych <andriy.khomych@gmail.com>
2019-11-18 12:09:21 +0200

    Fix redis php-7 build.

--- extensions/1_php/3_cache/2_redis.sh
+++ extensions/1_php/3_cache/2_redis.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 apk add redis
-cd /temp_docker && git clone https://github.com/phpredis/phpredis.git && cd phpredis && \
-git checkout php7-ipv6 && git pull && \
-phpize  && \
-./configure  && \
-make && \
-make install && \
+
+cd /temp_docker && wget https://github.com/phpredis/phpredis/archive/5.1.1.tar.gz
+cd /temp_docker && tar -xvzf 5.1.1.tar.gz && ls
+cd /temp_docker && cd phpredis-5.1.1 && phpize
+cd /temp_docker && cd phpredis-5.1.1 && ./configure
+cd /temp_docker && cd phpredis-5.1.1 && make
+cd /temp_docker && cd phpredis-5.1.1 && make install && \
 echo 'extension=redis.so' > /etc/php7/conf.d/redis.ini
+cd /temp_docker && cd phpredis-5.1.1 && make test
 
